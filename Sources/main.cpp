4f202dc435223f2730eaf1c612af94158409e5db
Yang Kyowon <kyowonhorang@gmail.com>
2022-04-27 18:11:13 +0900

    Initialize Commit

---
+++ Sources/main.cpp
@@ -0,0 +1,13 @@
+#include <taskflow/taskflow.hpp>
+#include <imgui/imgui.h>
+#include <d3d12.h>
+#include <d3dx12.h>
+#include <D3D12MemoryAllocator/D3D12MemAlloc.h>
+#include <assimp/Importer.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
