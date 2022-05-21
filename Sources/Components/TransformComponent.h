d4ab150038ea0a5a095f679c8cb71fd3782e9f66
Yang Kyowon <kyowonhorang@gmail.com>
2022-05-22 01:44:14 +0900

    Added LocalMatrix, WorldMatrix function to TransformComponent

--- Sources/Components/TransformComponent.h
+++ Sources/Components/TransformComponent.h
@@ -9,6 +9,27 @@ namespace sy
         DirectX::XMFLOAT3 LocalPosition = DirectX::XMFLOAT3();
         DirectX::XMFLOAT3 LocalScale = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f);
         DirectX::XMFLOAT4 LocalRotation = DirectX::XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f);
+
+        DirectX::XMMATRIX LocalMatrix() const
+        {
+            /** SRT(Pre-multiplication with row vector) */
+            DirectX::XMVECTOR localPosition { };
+            DirectX::XMLoadFloat3(&LocalPosition);
+            DirectX::XMVECTOR localScale { };
+            DirectX::XMLoadFloat3(&LocalScale);
+            DirectX::XMVECTOR localRotation { };
+            DirectX::XMLoadFloat4(&LocalRotation);
+
+            auto S = DirectX::XMMatrixScalingFromVector(localScale);
+            auto R = DirectX::XMMatrixRotationQuaternion(localRotation);
+            auto T = DirectX::XMMatrixTranslationFromVector(localPosition);
+            return DirectX::XMMatrixMultiply(DirectX::XMMatrixMultiply(S, R), T);
+        }
+
+        DirectX::XMMATRIX WorldMatrix(DirectX::XMMATRIX parentWorldMatrix) const
+        {
+            return DirectX::XMMatrixMultiply(parentWorldMatrix, LocalMatrix());
+        }
     };
 
     DeclareComponent(TransformComponent);
