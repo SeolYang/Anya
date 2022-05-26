26dc35e1343d714904f73e213359482ae8650f51
Yang Kyowon <kyowonhorang@gmail.com>
2022-05-26 20:29:45 +0900

    Added typed Descriptor Heap classes

--- Sources/RHI/DescriptorHeap.cpp
+++ Sources/RHI/DescriptorHeap.cpp
@@ -5,8 +5,10 @@
 
 namespace sy
 {
-	DescriptorHeap::DescriptorHeap(const Device& device, D3D12_DESCRIPTOR_HEAP_TYPE heapType, uint32_t numDescriptors)
+	DescriptorHeap::DescriptorHeap(const Device& device, const D3D12_DESCRIPTOR_HEAP_TYPE heapType, const uint32_t numDescriptors) :
+		numOfDescriptors(numDescriptors)
 	{
+		// Shaer Visibility of Descriptor Heap: https://seolyang.tistory.com/35 -> 리소스 뷰 분류 참고
 		bool bIsVisibleToShader = (heapType == D3D12_DESCRIPTOR_HEAP_TYPE::D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV) || (heapType == D3D12_DESCRIPTOR_HEAP_TYPE::D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER);
 		D3D12_DESCRIPTOR_HEAP_DESC desc
 		{
@@ -17,6 +19,7 @@ namespace sy
 		};
 
 		DXCall(device.D3DDevice()->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&descriptorHeap)));
+		descriptorSize = device.D3DDevice()->GetDescriptorHandleIncrementSize(heapType);
 		SetDebugName(TEXT("DescriptorHeap"));
 	}
 
@@ -27,5 +30,25 @@ namespace sy
 		{
 			descriptorHeap->SetName(debugName.data());
 		}
+	}
+	
+	CBVSRVUAVDescriptorHeap::CBVSRVUAVDescriptorHeap(const Device& device, const uint32_t numOfDescriptors) :
+		DescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, numOfDescriptors)
+	{
+	}
+
+	SamplerDescriptorHeap::SamplerDescriptorHeap(const Device& device, const uint32_t numDescriptors) :
+		DescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER, numDescriptors)
+	{
+	}
+
+	DSVDescriptorHeap::DSVDescriptorHeap(const Device& device, const uint32_t numDescriptors) :
+		DescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_DSV, numDescriptors)
+	{
+	}
+
+	RTVDescriptorHeap::RTVDescriptorHeap(const Device& device, const uint32_t numDescriptors) :
+		DescriptorHeap(device, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, numDescriptors)
+	{
 	}
 }
--- Sources/RHI/DescriptorHeap.h
+++ Sources/RHI/DescriptorHeap.h
@@ -8,7 +8,7 @@ namespace sy
 	class DescriptorHeap : public RHIObject
 	{
 	public:
-		DescriptorHeap(const Device& device, D3D12_DESCRIPTOR_HEAP_TYPE heapType, uint32_t numDescriptors);
+		DescriptorHeap(const Device& device, const D3D12_DESCRIPTOR_HEAP_TYPE heapType, const uint32_t numDescriptors);
 
 		auto D3DDescriptorHeap() const noexcept { return descriptorHeap.Get(); }
 
@@ -16,6 +16,48 @@ namespace sy
 
 	private:
 		ComPtr<ID3D12DescriptorHeap> descriptorHeap;
+		size_t descriptorSize;
+		size_t numOfDescriptors;
+
+	};
+
+	/**
+	* @brief Descriptor Heap for CBV(Constant Buffer View), SRV(Shader Resource View), and UAV(Unordered Access View)
+	*/
+	class CBVSRVUAVDescriptorHeap : public DescriptorHeap
+	{
+	public:
+		CBVSRVUAVDescriptorHeap(const Device& device, const uint32_t numDescriptors);
+
+	};
+
+	/**
+	* @brief Descriptor Heap for Sampler.
+	*/
+	class SamplerDescriptorHeap : public DescriptorHeap
+	{
+	public:
+		SamplerDescriptorHeap(const Device& device, const uint32_t numDescriptors);
+
+	};
+
+	/**
+	* @brief Descriptor Heap for DSV(Depth-Stencil View).
+	*/
+	class DSVDescriptorHeap : public DescriptorHeap
+	{
+	public:
+		DSVDescriptorHeap(const Device& device, const uint32_t numDescriptors);
+
+	};
+
+	/**
+	* @brief Descriptor Heap for RTV(Render Target View).
+	*/
+	class RTVDescriptorHeap : public DescriptorHeap
+	{
+	public:
+		RTVDescriptorHeap(const Device& device, const uint32_t numDescriptors);
 
 	};
 }
