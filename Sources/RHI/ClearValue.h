c4c52807c7370d687d79f6a103ad0bd66e159244
Yang Kyowon <kyowonhorang@gmail.com>
2022-05-27 22:44:00 +0900

    Setup Initial Value for ClearValue

--- Sources/RHI/ClearValue.h
+++ Sources/RHI/ClearValue.h
@@ -6,32 +6,32 @@ namespace sy
 	class ClearValue
 	{
 	public:
-		ClearValue(DXGI_FORMAT format, DirectX::XMFLOAT4 color, float depth, uint8_t stencil) :
-			format(format),
-			color(color),
-			depth(depth),
-			stencil(stencil)
+		ClearValue(DXGI_FORMAT format, DirectX::XMFLOAT4 color = DirectX::XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f), float depth = 1.0f, uint8_t stencil = 0) :
+			Format(format),
+			Color(color),
+			Depth(depth),
+			Stencil(stencil)
 		{
 		}
 
 		inline D3D12_CLEAR_VALUE D3DClearValue() const
 		{
 			D3D12_CLEAR_VALUE output;
-			output.Format = format;
-			output.Color[0] = color.x;
-			output.Color[1] = color.y;
-			output.Color[2] = color.z;
-			output.Color[3] = color.w;
-			output.DepthStencil.Depth = depth;
-			output.DepthStencil.Stencil = stencil;
+			output.Format = Format;
+			output.Color[0] = Color.x;
+			output.Color[1] = Color.y;
+			output.Color[2] = Color.z;
+			output.Color[3] = Color.w;
+			output.DepthStencil.Depth = Depth;
+			output.DepthStencil.Stencil = Stencil;
 			return output;
 		}
 
 	public:
-		DXGI_FORMAT format;
-		DirectX::XMFLOAT4 color = { 0.0f, 0.0f, 0.0f, 0.0f };
-		float depth = 1.0f;
-		uint8_t stencil = 0;
+		DXGI_FORMAT Format;
+		DirectX::XMFLOAT4 Color;
+		float Depth;
+		uint8_t Stencil;
 
 	};
 }
